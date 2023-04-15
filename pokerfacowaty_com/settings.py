fde8948fb7960c82f544bbcdf428929698657056
PokerFacowaty <pokerfacowaty.bn@gmail.com>
2023-04-15 14:44:42 +0000

    Fixed the parsing of Debug environment variable

--- pokerfacowaty_com/settings.py
+++ pokerfacowaty_com/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ['DEBUG']
+DEBUG = os.environ['DEBUG'] == 'True'
 
 ALLOWED_HOSTS = ['.pokerfacowaty.com']
 CSRF_TRUSTED_ORIGINS = ['https://*.pokerfacowaty.com']
