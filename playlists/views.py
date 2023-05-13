30b55d7d7eddffcb2dd22e1c57355f76d6bc0724
PokerFacowaty <pokerfacowaty.bn@gmail.com>
2023-05-13 21:00:50 +0000

    Added playlist view

--- playlists/views.py
+++ playlists/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from .models import Playlist
+from django.conf import settings
+from pathlib import Path
 
 
 def index(request):
-    return render(request, 'playlists/base_main.html')
+    playlists = Playlist.objects.filter(VISIBILITY='PU').order_by('TITLE')
+    csv_paths = []
+    for playlist in playlists:
+        csv_paths.append(str(Path(settings.PLAYLIST_CSV_DIR
+                                  / playlist.CSV_FILENAME)))
+    return render(request, 'playlists/base_main.html',
+                  {'csv_paths': csv_paths})
