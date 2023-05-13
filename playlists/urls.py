9c641b2a6d62ea9746f94c99a538c7e8ba6ba555
PokerFacowaty <pokerfacowaty.bn@gmail.com>
2023-05-13 22:28:50 +0000

    Made it so that the playlists app works as intended on with a single playlist in the db; made a site listing all playlists that are links to single pages parsing csv files

--- playlists/urls.py
+++ playlists/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('<str:short>', views.playlist, name='playlist'),
     path('', views.index, name='playlists')
 ]
--- playlists/views.py
+++ playlists/views.py
@@ -1,14 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Playlist
 from django.conf import settings
 from pathlib import Path
+import csv
 
 
 def index(request):
     playlists = Playlist.objects.filter(VISIBILITY='PU').order_by('TITLE')
-    csv_paths = []
-    for playlist in playlists:
-        csv_paths.append(str(Path(settings.PLAYLIST_CSV_DIR
-                                  / playlist.CSV_FILENAME)))
+    # all_csvs = []
+    # for pl in playlists:
+    #     with open(Path(settings.PLAYLIST_CSV_DIR) / pl.CSV_FILENAME,
+    #               encoding="utf-8") as f:
+    #         rdr = csv.DictReader(f, delimiter=";")
+    #         parsed_csv = list(rdr)
+    #     all_csvs.append(parsed_csv)
+
+    # return render(request, 'playlists/base_main.html',
+    #               {'all_csvs': all_csvs})
     return render(request, 'playlists/base_main.html',
-                  {'csv_paths': csv_paths})
+                  {'playlists': playlists})
+
+
+def playlist(request, short):
+    if (Playlist.objects.get(SHORT_TITLE=short).VISIBILITY == "PR"
+       and not request.user.is_authenticated):
+        return redirect("/admin/")
+
+    playlist = Playlist.objects.get(SHORT_TITLE=short)
+    with open(Path(settings.PLAYLIST_CSV_DIR) / playlist.CSV_FILENAME, 
+              encoding="utf-8") as f:
+        rdr = csv.DictReader(f, delimiter=";")
+        parsed_csv = list(rdr)
+    return render(request, 'playlists/base_playlist.html',
+                  {"playlist": playlist, 'csv': parsed_csv})
